9ed5441e4d941155cb336e3d8dfb7965e3fe6886
ardammr <arda.mamur@voids.ai>
2023-01-28 15:37:34 +0100

    updates

--- models/loss.py
+++ models/loss.py
@@ -6,9 +6,9 @@ import warnings
 warnings.simplefilter("ignore")
 
 def dice_coef_metric(probabilities: torch.Tensor,
-                     truth: torch.Tensor,
-                     treshold: float = 0.5,
-                     eps: float = 1e-9) -> np.ndarray:
+                    truth: torch.Tensor,
+                    treshold: float = 0.5,
+                    eps: float = 1e-9) -> np.ndarray:
     """
     Calculate Dice score for data batch.
     Params:
@@ -30,14 +30,14 @@ def dice_coef_metric(probabilities: torch.Tensor,
         if truth_.sum() == 0 and prediction.sum() == 0:
             scores.append(1.0)
         else:
-            scores.append((intersection + eps) / union)
-    return np.mean(scores)
-
+            scores.append((intersection) / (union + eps))
+    #return np.mean(scores)
+    return scores
 
 def jaccard_coef_metric(probabilities: torch.Tensor,
-               truth: torch.Tensor,
-               treshold: float = 0.5,
-               eps: float = 1e-9) -> np.ndarray:
+            truth: torch.Tensor,
+            treshold: float = 0.5,
+            eps: float = 1e-9) -> np.ndarray:
     """
     Calculate Jaccard index for data batch.
     Params:
@@ -60,16 +60,22 @@ def jaccard_coef_metric(probabilities: torch.Tensor,
         if truth_.sum() == 0 and prediction.sum() == 0:
             scores.append(1.0)
         else:
-            scores.append((intersection + eps) / union)
-    return np.mean(scores)
-
+            scores.append((intersection) / (union + eps))
+    #return np.mean(scores)
+    return scores
 
 class Meter:
     '''factory for storing and updating iou and dice scores.'''
     def __init__(self, treshold: float = 0.5):
         self.threshold: float = treshold
-        self.dice_scores: list = []
-        self.iou_scores: list = []
+        #self.dice_scores: list = []
+        self.dice_WT: list  = []
+        self.dice_TC: list = []
+        self.dice_ET: list = []
+        self.iou_WT: list = []
+        self.iou_TC: list = []
+        self.iou_ET: list = []
+        #self.iou_scores: list = []
     
     def update(self, logits: torch.Tensor, targets: torch.Tensor):
         """
@@ -80,17 +86,39 @@ class Meter:
         dice = dice_coef_metric(probs, targets, self.threshold)
         iou = jaccard_coef_metric(probs, targets, self.threshold)
         
-        self.dice_scores.append(dice)
-        self.iou_scores.append(iou)
+        self.dice_WT.append(dice[0])
+        self.dice_TC.append(dice[1])
+        self.dice_ET.append(dice[2])
+
+        self.iou_WT.append(iou[0])
+        self.iou_TC.append(iou[1])
+        self.iou_ET.append(iou[2])
+
+        #self.dice_scores.append(dice)
+        #self.iou_scores.append(iou)
     
     def get_metrics(self) -> np.ndarray:
         """
         Returns: the average of the accumulated dice and iou scores.
         """
-        dice = np.mean(self.dice_scores)
-        iou = np.mean(self.iou_scores)
-        return dice, iou
+        dice_score_WT = np.mean(self.dice_WT)
+        dice_score_TC = np.mean(self.dice_TC)
+        dice_score_ET = np.mean(self.dice_ET)
 
+        iou_score_WT = np.mean(self.iou_WT)
+        iou_score_TC = np.mean(self.iou_TC)
+        iou_score_ET = np.mean(self.dice_ET)
+
+        metrics = {
+            "dice_WT" : dice_score_WT,
+            "dice_TC" : dice_score_TC,
+            "dice_ET" : dice_score_ET,
+            "iou_WT"  : iou_score_WT,
+            "iou_TC" : iou_score_TC,
+            "iou_ET" : iou_score_ET
+        }
+
+        return metrics
 
 class DiceLoss(nn.Module):
     """Calculate dice loss."""
@@ -113,6 +141,41 @@ class DiceLoss(nn.Module):
         dice_score = (intersection + self.eps) / union
         #print("intersection", intersection, union, dice_score)
         return 1.0 - dice_score
+
+
+class DiceLossV2(nn.Module):
+    def __init__(self, sigmoid=True, mode="sum", eps=1e-7) -> None:
+        super().__init__()
+        self.sigmoid = sigmoid
+        self.mode = mode
+        self.eps = 1e-7
+
+    def forward(self, pred: torch.Tensor, target: torch.Tensor):
+        pred = torch.sigmoid(pred) if self.sigmoid else pred
+        batches, channels = pred.shape[:2]
+
+        if self.mode == "sum":
+            intersect = pred * target
+            union = pred**2 + target**2
+            dice_coeff = 2*intersect.sum() / (union.sum()+self.eps)
+        
+        elif self.mode == "sum_channel_avg_batch":
+            pred = pred.view(batches, -1)
+            target = target.view(batches, -1)
+            intersect = pred * target
+            union = pred**2 + target**2
+            dice_coeff = 2*intersect.sum(-1) / (union.sum(-1)+self.eps)
+            dice_coeff = dice_coeff.mean()
+        
+        elif self.mode == "average_channel_batch":
+            pred = pred.view(batches, channels, -1)
+            target = target.view(batches, channels, -1)
+            intersect = pred * target
+            union = pred**2 + target**2
+            dice_coeff = 2*intersect.sum(-1) / (union.sum(-1)+self.eps)
+            dice_coeff = dice_coeff.mean()
+
+        return 1-dice_coeff
         
         
 class BCEDiceLoss(nn.Module):
@@ -160,7 +223,7 @@ def dice_coef_metric_per_classes(probabilities: np.ndarray,
             intersection = 2.0 * (truth_ * prediction).sum()
             union = truth_.sum() + prediction.sum()
             if truth_.sum() == 0 and prediction.sum() == 0:
-                 scores[classes[class_]].append(1.0)
+                scores[classes[class_]].append(1.0)
             else:
                 scores[classes[class_]].append((intersection + eps) / union)
                 
@@ -168,10 +231,10 @@ def dice_coef_metric_per_classes(probabilities: np.ndarray,
 
 
 def jaccard_coef_metric_per_classes(probabilities: np.ndarray,
-               truth: np.ndarray,
-               treshold: float = 0.5,
-               eps: float = 1e-9,
-               classes: list = ['WT', 'TC', 'ET']) -> np.ndarray:
+            truth: np.ndarray,
+            treshold: float = 0.5,
+            eps: float = 1e-9,
+            classes: list = ['WT', 'TC', 'ET']) -> np.ndarray:
     """
     Calculate Jaccard index for data batch and for each class.
     Params:
@@ -195,7 +258,7 @@ def jaccard_coef_metric_per_classes(probabilities: np.ndarray,
             intersection = (prediction * truth_).sum()
             union = (prediction.sum() + truth_.sum()) - intersection + eps
             if truth_.sum() == 0 and prediction.sum() == 0:
-                 scores[classes[class_]].append(1.0)
+                scores[classes[class_]].append(1.0)
             else:
                 scores[classes[class_]].append((intersection + eps) / union)
 
--- train.py
+++ train.py
@@ -15,16 +15,16 @@ import time
 import pandas as pd
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print("device:", device)
+
 
 class Train():
     def __init__(self, hyper_parameters, model_name):
-        if model_name == "3dunet":
-            self.model =  UNet3d(in_channels=hyper_parameters["in_channels"], n_classes=3, n_channels=hyper_parameters['init_channels'])
-        else: # implememnt it later
-            #model_name == "double_unet"
-            self.model =  DoubleUNet3d(in_channels=hyper_parameters["in_channels"], n_classes=3)
         self.model_name = model_name
+        if self.model_name == "3dunet":
+            self.model =  UNet3d(in_channels=hyper_parameters["in_channels"], n_classes=3, n_channels=hyper_parameters['init_channels'])
+        else:
+            self.model =  DoubleUNet3d(in_channels=hyper_parameters["in_channels"], n_classes=3)
+        self.model =  UNet3d(in_channels=hyper_parameters["in_channels"], n_classes=3, n_channels=hyper_parameters['init_channels'])
         self.model = self.model.to(device)
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=hyper_parameters['lr'], weight_decay=hyper_parameters['weight_decay'])
         self.scheduler = PolyLR(self.optimizer, max_epoch=hyper_parameters['num_epochs'], power=0.9)
@@ -32,12 +32,16 @@ class Train():
         self.phases = ["train", "val"]
         self.num_epochs = hyper_parameters["num_epochs"]
         self.accumulation_steps = hyper_parameters['accumulation_steps']
-        self.loaders = make_data_loaders(mode="train", model_name=model_name)
+        self.loaders = make_data_loaders(mode="train", model_name=self.model_name)
         self.best_loss = float("inf")
         self.n_steps = hyper_parameters['n_steps']
         self.losses = {phase: [] for phase in self.phases}
-        self.dice_scores = {phase: [] for phase in self.phases}
-        self.jaccard_scores = {phase: [] for phase in self.phases}
+        self.dice_scores_WT = {phase: [] for phase in self.phases}
+        self.dice_scores_TC = {phase: [] for phase in self.phases}
+        self.dice_scores_ET = {phase: [] for phase in self.phases}
+        self.jaccard_scores_WT = {phase: [] for phase in self.phases}
+        self.jaccard_scores_TC = {phase: [] for phase in self.phases}
+        self.jaccard_scores_ET = {phase: [] for phase in self.phases}
 
     def _compute_loss_and_outputs(self, images: torch.Tensor, targets: torch.Tensor):
         images = images.to(device)
@@ -77,29 +81,45 @@ class Train():
             running_loss += loss.item()
             meter.update(logits.detach().cpu(),targets.detach().cpu())
             if (itr + 1) % self.n_steps == 0:
-                print("loss: ", loss.item())
+                print("BCEDice loss: ", loss.item())
         
         epoch_loss = (running_loss * self.accumulation_steps) / total_batches
-        epoch_dice, epoch_iou = meter.get_metrics()
-        print("Accumulated loss: ", epoch_loss, ", Dice loss: ", epoch_dice, ", IoU loss: " , epoch_iou )
+        metrics = meter.get_metrics()
+        print("###################################")
+        print("accumulated loss: ", epoch_loss)
+
+        print("WT dice: " , metrics['dice_WT'], 
+            " TC dice: " , metrics['dice_TC'] ,
+            " ET dice: ", metrics['dice_ET'])
+
+        print("WT jaccard: ", metrics['iou_WT'],
+            " TC jaccard: ", metrics['iou_TC'],
+            " ET jaccard: ", metrics['iou_ET'])
+
+        print("###################################")
+        
         self.losses[phase].append(epoch_loss)
-        self.dice_scores[phase].append(epoch_dice)
-        self.jaccard_scores[phase].append(epoch_iou)
+        self.dice_scores_WT[phase].append(metrics['dice_WT'])
+        self.dice_scores_TC[phase].append(metrics['dice_TC'])
+        self.dice_scores_ET[phase].append(metrics['dice_ET'])
+
+        self.jaccard_scores_WT[phase].append(metrics['iou_WT'])
+        self.jaccard_scores_TC[phase].append(metrics['iou_TC'])
+        self.jaccard_scores_ET[phase].append(metrics['iou_ET'])
         
         return epoch_loss
 
 
     def run(self):
         for epoch in range(self.num_epochs):
-            if self.model_name != "3dunet":
-                self.model.enc1.requires_grad_(False)
             self._do_epoch(epoch, "train")
             with torch.no_grad():
                 val_loss = self._do_epoch(epoch, "val")
                 self.scheduler.step(val_loss)
-                                
+            if self.display_plot:
+                self._plot_train_history()
             if val_loss < self.best_loss:
-                print(f"\n{'#'*20}\nSaved new checkpoint\n{'#'*20}\n")
+                print("Saved new checkpoint")
                 self.best_loss = val_loss
                 torch.save(self.model.state_dict(), "best_model.pth")
             print()
@@ -108,12 +128,22 @@ class Train():
 
     def _save_train_history(self):
         """writing model weights and training logs to files."""
-        torch.save(self.model.state_dict(),f"last_epoch_model.pth")
+        torch.save(self.model.state_dict(),
+                f"last_epoch_model.pth")
 
-        logs_ = [self.losses, self.dice_scores, self.jaccard_scores]
-        log_names_ = ["_loss", "_dice", "_jaccard"]
-        logs = [logs_[i][key] for i in list(range(len(logs_))) for key in logs_[i]]
-        log_names = [key+log_names_[i] for i in list(range(len(logs_))) for key in logs_[i]]
+        logs_ = [self.losses,
+                self.dice_scores_WT, self.dice_scores_TC, self.dice_scores_ET,
+                self.jaccard_scores_WT, self.jaccard_scores_TC, self.jaccard_scores_ET]
+
+        log_names_ = ["_loss", "_dice_WT", "_dice_TC", "dice_ET",
+                    "_jaccard_WT", "_jaccard_TC", "_jaccard_ET"]
+
+        logs = [logs_[i][key] for i in list(range(len(logs_)))
+                        for key in logs_[i]]
+        log_names = [key+log_names_[i] 
+                    for i in list(range(len(logs_))) 
+                    for key in logs_[i]
+                    ]
         pd.DataFrame(
             dict(zip(log_names, logs))
         ).to_csv("train_log.csv", index=False)
@@ -123,7 +153,7 @@ class Train():
         print("Predtrain model loaded")
 
 
-def main(model_name):
+def main():
     config = configparser.ConfigParser()
     config.read('config.ini')
     params = config['params']
@@ -141,13 +171,13 @@ def main(model_name):
         "shapes" : shapes,
         "num_epochs" : int(params['num_epochs']),
         "input_shape" : (int(shapes[0]), int(shapes[1]), int(shapes[2])),
-        "accumulation_steps" : int(params['accumulation_steps']) / int(params['batch_size']),
+        "accumulation_steps" : 4 / int(params['batch_size']),
         "n_steps" : 5
     
     }
     print("hyper parameters:", hyper_parameters)
     
-    trainer = Train(hyper_parameters=hyper_parameters, model_name=model_name)
+    trainer = Train(hyper_parameters=hyper_parameters, model_name="double_unet")
     
     if params['pre_trained'] == "True":
         trainer.load_predtrain_model(params['pre_trained_path'])
@@ -156,15 +186,28 @@ def main(model_name):
         train_logs = pd.read_csv("train_log.csv")
         trainer.losses["train"] =  train_logs.loc[:, "train_loss"].to_list()
         trainer.losses["val"] =  train_logs.loc[:, "val_loss"].to_list()
-        trainer.dice_scores["train"] = train_logs.loc[:, "train_dice"].to_list()
-        trainer.dice_scores["val"] = train_logs.loc[:, "val_dice"].to_list()
-        trainer.jaccard_scores["train"] = train_logs.loc[:, "train_jaccard"].to_list()
-        trainer.jaccard_scores["val"] = train_logs.loc[:, "val_jaccard"].to_list()
 
-    print("START TRAINING")
+
+        trainer.dice_scores_WT["train"] = train_logs.loc[:, "train_dice_WT"].to_list()
+        trainer.dice_scores_TC["train"] = train_logs.loc[:, "train_dice_TC"].to_list()
+        trainer.dice_scores_ET["train"] = train_logs.loc[:, "train_dice_ET"].to_list()
+
+        trainer.dice_scores_WT["val"] = train_logs.loc[:, "val_dice_WT"].to_list()
+        trainer.dice_scores_TC["val"] = train_logs.loc[:, "val_dice_TC"].to_list()
+        trainer.dice_scores_ET["val"] = train_logs.loc[:, "val_dice_ET"].to_list()
+
+
+        trainer.jaccard_scores_WT["train"] = train_logs.loc[:, "train_jaccard_WT"].to_list()
+        trainer.jaccard_scores_TC["train"] = train_logs.loc[:, "train_jaccard_TC"].to_list()
+        trainer.jaccard_scores_ET["train"] = train_logs.loc[:, "train_jaccard_ET"].to_list()
+
+        trainer.jaccard_scores_TC["val"] = train_logs.loc[:, "val_jaccard"].to_list()
+        trainer.jaccard_scores_TC["val"] = train_logs.loc[:, "val_jaccard"].to_list()
+        trainer.jaccard_scores_ET["val"] = train_logs.loc[:, "val_jaccard"].to_list()
+
+    print("START TRAINING")         
     trainer.run()
     print("TRAINING DONE. LOGS ARE SAVED..")
 
 
-main(model_name="3dunet")
-
+main()
