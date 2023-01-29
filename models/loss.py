2b7eee4e2eec95dad28d55958792c8076af3d2af
ardammr <arda.mamur@voids.ai>
2023-01-29 21:55:28 +0100

    BCEDiceLOSS_V2 added

--- models/loss.py
+++ models/loss.py
@@ -223,8 +223,47 @@ class DiceLossV2(nn.Module):
             dice_coeff = dice_coeff.mean()
 
         return 1-dice_coeff
+
+
+class BCEDiceLossV2(nn.Module):
+    def __init__(self, sigmoid=True, mode="average_channel_batch", eps=1e-7) -> None:
+        super().__init__()
+        self.sigmoid = sigmoid
+        self.bce = nn.BCEWithLogitsLoss() # This loss combines a Sigmoid layer and the BCELoss in one single class 
+        self.mode = mode
+        self.eps = 1e-7
+
+    def forward(self, pred: torch.Tensor, target: torch.Tensor):
+        bce_loss = self.bce(pred, target)
+        pred = torch.sigmoid(pred) if self.sigmoid else pred
+        batches, channels = pred.shape[:2]
+
+        if self.mode == "sum":
+            intersect = pred * target
+            union = pred**2 + target**2
+            dice_coeff = 2*intersect.sum() / (union.sum()+self.eps)
         
+        elif self.mode == "sum_channel_avg_batch":
+            pred = pred.view(batches, -1)
+            target = target.view(batches, -1)
+            intersect = pred * target
+            union = pred**2 + target**2
+            dice_coeff = 2*intersect.sum(-1) / (union.sum(-1)+self.eps)
+            dice_coeff = dice_coeff.mean()
         
+        elif self.mode == "average_channel_batch":
+            pred = pred.view(batches, channels, -1)
+            target = target.view(batches, channels, -1)
+            intersect = pred * target
+            union = pred**2 + target**2
+            dice_coeff = 2*intersect.sum(-1) / (union.sum(-1)+self.eps)
+            dice_coeff = dice_coeff.mean()
+
+        dice_loss = 1 - dice_coeff
+
+        return dice_loss + bce_loss
+
+
 class BCEDiceLoss(nn.Module):
     """Compute objective loss: BCE loss + DICE loss."""
     def __init__(self):
